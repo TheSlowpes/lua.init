60226ec3c8e711b9903589fb2c3494733c46e5d3
Kevin Lopes <slowpes@gmail.com>
2025-06-23 07:54:24 -0400

    Configuring codecompanion to use copilot and gemini only for now, also properly configured some of the keymaps for autocomplete

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -3,37 +3,54 @@ return {
 		"zbirenbaum/copilot.lua",
 		cmd = "Copilot",
 		event = "InsertEnter",
-		opts = {
-			panel = {
-				enabled = true,
-				auto_refresh = true,
-			},
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				keymaps = {
-					accept = "<C-y>",
-					accept_word = "<C-l>",
-					accept_line = "<C-S-j>",
-					next = "<C-j>",
-					previous = "<C-k>",
-					dismiss = "<C-e>",
+		config = function()
+			require("copilot").setup({
+				suggestion = {
+					enabled = true,
+					auto_trigger = true,
+					debounce = 75,
+					keymap = {
+						accept = "<S-TAB>",
+						accept_word = "<C-l>",
+						accept_line = "<C-j>",
+						dismiss = "<C-k>",
+					},
 				},
-			},
-		},
+			})
+		end,
 	},
 	{
 		"olimorris/codecompanion.nvim",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
-		opts = {
-			keymaps = {},
-		},
-		config = function(_, opts)
-			require("codecompanion").setup(opts)
+		config = function()
+			require("codecompanion").setup({
+				adapters = {
+					opts = { show_defaults = false, show_model_choices = true },
+					copilot = function()
+						return require("codecompanion.adapters").extend("copilot", {
+							schema = {
+								model = {
+									default = "claude-3.5-sonnet",
+								},
+							},
+						})
+					end,
+					gemini = function()
+						return require("codecompanion.adapters").extend("gemini", {
+							env = { api_key = os.getenv("GEMINI_API_KEY") },
+							schema = {
+								model = {
+									default = "gemini-2.5-flash-preview-05-20",
+								},
+							},
+						})
+					end,
+				},
+			})
 			vim.keymap.set("n", "<leader>cc", "<cmd>CodeCompanionChat Toggle<cr>", { desc = "Code Companion: Chat" })
-			vim.keymap.set("n", "<leader>ci", "<cmd>CodeCompanion<cr>", { desc = "Code Companion: Inline Chat" })
+			vim.keymap.set("v", "<leader>ci", "<cmd>CodeCompanion<cr>", { desc = "Code Companion: Inline Chat" })
 		end,
 	},
 }
